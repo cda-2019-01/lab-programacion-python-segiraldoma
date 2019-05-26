b9da9b73a8cec7ac4bba692c5a7373a6ca3ae698
segiraldoma <50236398+segiraldoma@users.noreply.github.com>
2019-05-26 15:52:51 -0500

    Final

--- q01.py
+++ q01.py
@@ -1,3 +1,6 @@
-##
 ## Imprima la suma de la segunda columna.
-##
+x = open('data.csv','r').readlines() #Se lee el archivo
+x = [z.replace('\n', '') for z in x] #Quitamos retorno de carro
+x = [z.split('\t') for z in x] #Quitamos espacios 
+a=sum([int(z[1]) for z in x[0:]]) #Hacemos la operacion suma de los valores que estan en la posicion 2 
+print(a) #Imprimimos resultado
--- q03.py
+++ q03.py
@@ -1,7 +1,24 @@
 ##
-## Imprima la suma de la columna 2 por cada letra de la 
-## primera columna, ordneados alfabeticamente.
-##
+## Imprima la cantidad de registros por letra para la
+## primera columna, ordenados alfabeticamente.
+x = open('data.csv','r').readlines() #Se lee el archivo
+x = [z.replace('\n', '') for z in x]
+x = [z.split('\t') for z in x]
+b = [(z[0]) for z in x[0:]]
+e=[int(z[1]) for z in x[0:]]
+c = set(b)
+d = list(c)
+d.sort()
+lenb = len(b)
+lend = len(d)
+for i in range(0,lend):
+    n=0
+    for j in range(0,lenb):
+        if b[j] == d[i]:
+            n = n + e[j]
+    print(d[i], end="")
+    print(',', end="")
+    print(n)
 ## A,37
 ## B,36
 ## C,27
